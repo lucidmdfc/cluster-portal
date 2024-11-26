6712612346fe33731d4a6693aca4974650d2b9ce
Ahmed Addy <ahmeddev.bz@gmail.com>
2024-11-26 18:47:58 +0100

    Add Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,36 @@
-import config from "eslint-config-standard";
+import globals from 'globals';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  ...[].concat(config),
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parser: '@typescript-eslint/parser', // Use TypeScript parser
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json', // Make sure the path to tsconfig is correct
+      },
+    },
+  },
+  ...compat.extends('standard-with-typescript'),
+  pluginReactConfig,
+  {
+    plugins: ['@typescript-eslint'], // Add TypeScript plugin
+    rules: {
+      '@typescript-eslint/lines-between-class-members': 'error',
+      '@typescript-eslint/no-throw-literal': 'error',
+    },
+  },
 ];
