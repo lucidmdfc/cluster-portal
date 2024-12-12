72dfc744be1eb989fad5d29975b30d87ec73c639
Ahmed Addy <ahmeddev.bz@gmail.com>
2024-12-12 17:36:30 +0100

    Remove form from careers

--- src/sections/_cluster-portal/_career/landing/career-landing-featured-jobs.tsx
+++ src/sections/_cluster-portal/_career/landing/career-landing-featured-jobs.tsx
@@ -25,57 +25,6 @@ type Props = {
 // };
 
 export default function CareerLandingFeaturedJobs({ jobs }: Props) {
-  // Define form fields configuration
-  const fields = [
-    {
-      name: 'firstName', // Field name
-      label: 'First Name', // Label displayed in the form
-      required: true, // Indicates if the field is required
-      type: 'text', // Input type
-    },
-    {
-      name: 'lastName', // Field name
-      label: 'Last Name', // Label displayed in the form
-      required: true, // Indicates if the field is required
-      type: 'text', // Input type
-    },
-    {
-      name: 'phone',
-      label: 'Phone Number',
-      required: true,
-      type: 'tel',
-    },
-    {
-      name: 'email',
-      label: 'Email Address',
-      required: true,
-      type: 'email',
-    },
-  ];
-
-  // Define Yup validation schema
-  const validationSchema = yup.object({
-    firstName: yup.string().required('First Name is required'),
-    lastName: yup.string().required('Last Name is required'),
-    phone: yup.string().required('Phone Number is required'),
-    email: yup.string().email('Invalid email address').required('Email Address is required'),
-  });
-
-  // Handle form submission
-  const handleSubmit = async (formData: FormData) => {
-
-    try {
-      // Here, you can send the formData to your backend API
-      handleFormSubmission(formData, "candidate")
-
-      // Display a success message
-      toast.success('Votre formulaire a été envoyé avec succès');
-    } catch (error) {
-      // Handle errors during form submission
-      console.error('Error submitting form:', error);
-      toast.error("Échec de l'envoi du formulaire, veuillez réessayer");
-    }
-  };
 
   return (
     <Container
@@ -130,38 +79,6 @@ export default function CareerLandingFeaturedJobs({ jobs }: Props) {
         >
           View All
         </Button>
-      </Stack>
-
-      {/* Account creation section */}
-      <Grid container spacing={{ xs: 2, md: 4 }} sx={{ textAlign: { xs: 'center', md: 'unset' }, mt: 5 }}>
-        <Grid xs={12} md={4}>
-          <Typography variant="overline" sx={{ color: 'text.disabled' }}>
-            Create Your Account
-          </Typography>
-        </Grid>
-        <Grid xs={12} md={8}>
-          <Typography variant="h4">
-            The information provided in this form will create your personal account. To modify your
-            details, simply refill the form. Your previous data will automatically be replaced.
-          </Typography>
-        </Grid>
-      </Grid>
-
-      {/* Form Section */}
-      <Stack
-        id="upload-cv"
-        alignItems="center"
-        direction="row"
-        justifyContent="center"
-        spacing={2}
-        sx={{ mt: 3 }}
-      >
-        <DynamicForm
-          fields={fields} // Pass form fields configuration
-          validationSchema={validationSchema} // Pass validation schema
-          onSubmit={handleSubmit} // Pass submit handler
-          isSubmitting={false} // Set to `true` during API calls for a loading state
-        />
       </Stack>
     </Container>
   );
